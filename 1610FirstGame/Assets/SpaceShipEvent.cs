5a4a53fe3532346e75cad307a14144b4d7ee24e6
Kaden <10836246@my.uvu.edu>
2019-01-30 11:12:57 -0700

    Added particle and physics

---
+++ 1610FirstGame/Assets/SpaceShipEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class SpaceShipEvent : MonoBehaviour
+{
+	public UnityEvent Event;
+	
+		private void OnCollisionEnter2D()	
+		{
+			Event.Invoke();
+		}
+	
+
+}
