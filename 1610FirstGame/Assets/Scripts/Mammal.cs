c3d78c0b3bceabec27f1c56a47d549b2ada5db76
Kaden <10836246@my.uvu.edu>
2019-02-06 11:22:55 -0700

    Learned rotation

--- 1610FirstGame/Assets/Scripts/Mammal.cs
+++ 1610FirstGame/Assets/Scripts/Mammal.cs
@@ -2,20 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fish : Animal
+public class Mammal : MonoBehaviour 
 {
-	public Color finColor;
-	
-
 	// Use this for initialization
-	void Start ()
+	void Start () 
 	{
-		GetComponent<SpriteRenderer>().color = skinColor;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.Rotate(0,1,0);
+		
 	}
 }
