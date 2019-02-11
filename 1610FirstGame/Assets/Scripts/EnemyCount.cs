904ce953fee6bc63e8259310f146ecc4b2c55b09
Kaden Campbell <10836246@my.uvu.edu>
2019-02-10 21:04:14 -0700

    Added Enemy Counter

---
+++ 1610FirstGame/Assets/Scripts/EnemyCount.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCount : MonoBehaviour
+{
+	public int currentEnemyAmount;
+	public int killCount;
+	public int enemyType;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
