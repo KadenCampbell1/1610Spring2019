ec10bb04986b1a2a2b5e23cace5b7ff6215c92cb
Kaden Campbell <10836246@my.uvu.edu>
2019-02-12 20:05:25 -0700

    Added WindowLights script

---
+++ 1610FirstGame/Assets/Scripts/WindowLights.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowLights : MonoBehaviour
+{
+	public bool onScreen;
+	public bool turnedOn;
+	public int randomChance;
+	public bool willTurnOff;
+	public bool willTurnOn;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
