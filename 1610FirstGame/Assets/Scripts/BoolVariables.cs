a47ea1237ba99fe79b3c7a25d18fc511c5caea9e
Kaden Campbell <10836246@my.uvu.edu>
2019-02-14 10:59:01 -0700

    Added a global Bool variable

---
+++ 1610FirstGame/Assets/Scripts/BoolVariables.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BoolVariables : ScriptableObject
+{
+    public bool globalBool;
+}
