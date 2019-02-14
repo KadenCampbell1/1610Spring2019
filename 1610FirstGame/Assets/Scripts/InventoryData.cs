42d39b13ad49a9d353c3d7a53e35ae83ac49fb4e
Kaden Campbell <10836246@my.uvu.edu>
2019-02-13 19:28:29 -0700

    Added 3 new Animal classes and scriptable object with instances
    updated home to Unity 3.5f1

---
+++ 1610FirstGame/Assets/Scripts/InventoryData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class InventoryData : ScriptableObject
+{
+   public int inventorySpace;
+   public int itemTotal;
+}
