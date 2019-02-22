77d0f4723e5774dea6461fcb649434f37ef0089b
Kaden Campbell <10836246@my.uvu.edu>
2019-02-22 13:07:36 -0700

    Added Save the World ScriptableObject and script

---
+++ 1610FirstGame/Assets/Scripts/NewStringData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class NewStringData : ScriptableObject
+{
+    public string newGlobalString;
+
+    private void OnEnable()
+    {
+        newGlobalString = "Save the World";
+    }
+}
