e3d2bd20af596bd71de18a57d42b12b58c265942
Kaden Campbell <10836246@my.uvu.edu>
2019-02-23 23:20:36 -0700

    Added a new Float ScriptableObject and instance of it

---
+++ 1610FirstGame/Assets/Scripts/NewFloatData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class NewFloatData : ScriptableObject
+{
+    public float newGlobalFloat;
+
+    private void OnEnable()
+    {
+        newGlobalFloat = 4.6f;
+    }
+}
