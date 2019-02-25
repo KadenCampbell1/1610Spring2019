ee1664a12378423060b49f01cf602c7b92656bf0
Kaden Campbell <10836246@my.uvu.edu>
2019-02-24 18:35:42 -0700

    Added a new Bool ScriptableObject and instance

---
+++ 1610FirstGame/Assets/Scripts/NewBoolData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class NewBoolData : ScriptableObject
+{
+    public bool newGlobalBoolData;
+
+    private void OnEnable()
+    {
+        newGlobalBoolData = true;
+    }
+}
