e1b730346c7f85c9f7b405fe562d468d99bb914a
Kaden <10836246@my.uvu.edu>
2019-02-25 11:02:42 -0700

    Learned If and Else

---
+++ 1610FirstGame/Assets/Scripts/Conditionals.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    public bool canWalk;
+    public int number = 11;
+    public string password;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (canWalk)
+        {
+            print("True");
+        }
+        else
+        {
+            print("False");
+        }
+
+        if (number >= 10)
+        {
+            print("greater");
+        }
+        else
+        {
+            print("less");
+        }
+
+        if (password == "Images")
+        {
+            print("and Words");
+        }
+        else
+        {
+            print("Over Time");
+        }
+        
+    }
+}
