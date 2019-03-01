d79ebc954fece45c88ca978edd4af34b905e83b5
Kaden Campbell <10836246@my.uvu.edu>
2019-03-01 11:06:18 -0700

    Made door and attempted rotation

---
+++ 1610FirstGame/Assets/Scripts/OpenObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OpenObject : MonoBehaviour
+{
+    public UnityEvent OpenEvent, CloseEvent;
+    
+    public bool openObjectBool;
+    //public Vector3 myVector3;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (openObjectBool)
+        {
+            OpenEvent.Invoke();
+        }
+        else
+        {
+            CloseEvent.Invoke();
+        }
+    }
+
+    public void RotateObject(Vector3 myVector3)
+    {
+        gameObject.transform.Rotate(myVector3);
+    }
+}
