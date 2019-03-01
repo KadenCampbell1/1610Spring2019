a4114dc88027fe9e71380aa85aab594884c1bb62
Kaden Campbell <10836246@my.uvu.edu>
2019-02-28 21:24:38 -0700

    Added GlobalCharacterData

---
+++ 1610FirstGame/Assets/Scripts/GlobalCharacterData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GlobalCharacterData : ScriptableObject
+{
+    public int playerHealth;
+    public int playerSpeed;
+    public int playerArmor;
+    public int playerPower;
+    public int playerJumpHeight;
+    public float playerBlockDistance;
+    
+    private void OnEnable()
+    {
+        playerHealth = 100;
+        playerArmor = 0;
+        playerSpeed = 15;
+        playerJumpHeight = 5;
+        playerBlockDistance = 5;
+        playerPower = 5;
+    }
+}
