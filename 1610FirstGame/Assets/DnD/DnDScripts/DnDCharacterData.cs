b90dd4e80157f3a7c22ea9adfe08a544080aa46e
Kaden Campbell <10836246@my.uvu.edu>
2019-03-02 13:13:31 -0700

    Added DnD Character scriptableObject and instances

---
+++ 1610FirstGame/Assets/DnD/DnDScripts/DnDCharacterData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DnDCharacterData : ScriptableObject
+{
+    //make a counter that runs through a while loop to roll multiple dice
+    public int numberOfDiceRolled;
+    //check if value was 20
+    public int valueOfDiceRolled;
+    //attack bonus and check bonuses
+    public int bonusValue;
+    //strength mod plus weapon bonus (+str+2 for gravedigger)
+    public int damageBonus;
+    //values to make damageBonus
+    public int strMod, weaponBonus;
+    //highest spell slot level to set up array
+    public int spellSlotMax;
+    public int[] spellSlots;
+    public int useSpellSlot;
+    public bool withinAuraOfHate;
+}
