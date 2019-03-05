21e82e6d0fb093b9176a74fdd55b7b40652b89c3
Kaden Campbell <10836246@my.uvu.edu>
2019-03-04 20:00:18 -0700

    Added TeamScript

---
+++ 1610FirstGame/Assets/Scripts/TeamScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TeamScript : ScriptableObject
+{
+    public int teamMembers;
+    public float comboDamage;
+    public string[] memberNames;
+    public string[] memberType;
+    
+}
