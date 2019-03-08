80e4305183befac98de1461e161d2011bdc8bb04
Kaden Campbell <10836246@my.uvu.edu>
2019-03-08 15:21:42 -0700

    Added multiplayer Script

---
+++ 1610FirstGame/Assets/Scripts/MultiPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiPlayer : MonoBehaviour
+{
+    public int numberCharacter;
+
+    public int buttonConfigNumber;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //see how many players are playing
+        //assign buttons for characters
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //see if someone drops out
+        //take characters off
+    }
+}
