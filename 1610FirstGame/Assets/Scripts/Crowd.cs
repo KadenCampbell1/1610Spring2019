d4e957e090eb0ce22861739bc3054310e2fefe7b
Kaden Campbell <10836246@my.uvu.edu>
2019-03-15 15:42:56 -0600

    added crowd script

---
+++ 1610FirstGame/Assets/Scripts/Crowd.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowd : MonoBehaviour
+{
+    public int crowdCapacity;
+
+    public Color personColor;
+
+    public Vector3 personPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        crowdCapacity = Random.Range(1, 100000);
+        //choose random color
+        //make seat range and assign random numbers between seat ranges
+        //make loop to assign position and color
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
