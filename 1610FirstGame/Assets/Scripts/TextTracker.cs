6ce7c5a4fb2709a7383332f010e061e9d47125a9
Kaden Campbell <10836246@my.uvu.edu>
2019-03-16 13:44:02 -0600

    Added KillCounter and TextTracker

---
+++ 1610FirstGame/Assets/Scripts/TextTracker.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TextTracker : ScriptableObject
+{
+    public int textCounter;
+    public string textName;
+    
+}
