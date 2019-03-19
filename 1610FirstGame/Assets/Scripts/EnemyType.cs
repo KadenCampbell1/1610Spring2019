fc87d4adec5db10d330d94260097b8f3d9eb2008
Kaden Campbell <10836246@my.uvu.edu>
2019-03-18 23:37:03 -0600

    Added EnemyType ScriptableObject

---
+++ 1610FirstGame/Assets/Scripts/EnemyType.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemyType : ScriptableObject
+{
+    public string enemyTypeName;
+    public int powerLevel;
+    public int difficultyLevel;
+    public int enemySpeed;
+
+}
