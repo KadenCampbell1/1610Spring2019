357029177f508c43c5f440d5766e139a7b92627a
Kaden Campbell <10836246@my.uvu.edu>
2019-03-20 14:21:27 -0600

    added Money scriptableObject

---
+++ 1610FirstGame/Assets/Scripts/Money.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Money : ScriptableObject
+{
+    public int moneyTotal;
+    public int moneyValue;
+
+    public void AddAmount()
+    {
+        moneyTotal += moneyValue;
+    }
+}
