9dd2699512f7f1da64d51a8e2495b19cbfcc5ab6
Kaden <10836246@my.uvu.edu>
2019-03-25 11:06:43 -0600

    Learned Lists and functions with parameters

---
+++ 1610FirstGame/Assets/Scripts/TodayScripts/Collection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Collection : ScriptableObject
+{
+    public List<Object> collectionList;
+    
+    public void Collect(Object obj)
+    {
+        collectionList.Add(obj);
+    }
+}
