8ca6fa2abddc4ea7f5b5012431263a5decb043fc
Kaden <10836246@my.uvu.edu>
2019-03-27 11:34:46 -0600

    Learned For and Foreach loops and made new scene for buttons

---
+++ 1610FirstGame/Assets/Scripts/ImportantScripts/Collection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ImportantScripts
+{
+    [CreateAssetMenu]
+    public class Collection : ScriptableObject
+    {
+        public List<FloatData> collectionList;
+    
+        public void Collect(FloatData obj)
+        {
+            collectionList.Add(obj);
+        }
+
+        public void CollectionInfo()
+        {
+            foreach (var obj in collectionList)
+            {
+                Debug.Log(obj);
+            }
+        }
+
+        public void AmmoInfo()
+        {
+            foreach (var obj in collectionList)
+            {
+                if (obj.name == "Ammo")
+                {
+                    Debug.Log("we have " + obj.Value + " Ammo.");
+                }
+            }
+        }
+
+        public void FuelInfo()
+        {
+            for (int i = 0; i < collectionList.Count; i++)
+            {
+                if (collectionList[i].name == "Fuel")
+                {
+                    Debug.Log(collectionList[i]);
+                }
+            }
+        }
+    }
+}
