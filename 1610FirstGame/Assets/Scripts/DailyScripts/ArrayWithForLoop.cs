780367ab6a953bae2180095b0cd60c3d687d71e1
Kaden Campbell <10836246@my.uvu.edu>
2019-03-29 22:58:12 -0600

    Figured out the Loops!

---
+++ 1610FirstGame/Assets/Scripts/DailyScripts/ArrayWithForLoop.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ImportantScripts;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ArrayWithForLoop : MonoBehaviour
+{
+    public FloatData[] itemsInPouch;
+    public Collection collectionScriptForArray;
+    public UnityEvent ArrayTriggerEvent;
+    public int itemTotal;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ArrayTriggerEvent.Invoke();
+    }
+
+    public void ArrayForLoop()
+    {
+        for (int i = 0; i < itemsInPouch.Length; i++)
+        {
+            collectionScriptForArray.collectionList.Add(itemsInPouch[i]);
+        }
+        
+        print("Pouch had " + ListTotal() + " Items in it.");
+    }
+
+    public void ArrayForEach()
+    {
+        foreach (var obj in itemsInPouch)
+        {
+            collectionScriptForArray.collectionList.Add(obj);
+        }
+        
+        print("Pouch had " + ListTotal() + " Items in it.");
+    }
+
+    private int ListTotal()
+    {
+        foreach (var obj in itemsInPouch)
+        {
+            itemTotal++;
+        }
+
+        return itemTotal;
+    }
+    
+}
