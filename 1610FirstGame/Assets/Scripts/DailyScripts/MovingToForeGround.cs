5368b7eb90472fd90d7710faf6406f151b785b3d
Kaden Campbell <10836246@my.uvu.edu>
2019-03-30 18:42:49 -0600

    Added MovingToForeground script

---
+++ 1610FirstGame/Assets/Scripts/DailyScripts/MovingToForeGround.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class MovingToForeGround : MonoBehaviour
+{
+    public Transform playerTransform;    
+    public Vector3 playerScale;
+    public bool movingForward;
+    
+    void Start()
+    {
+        playerScale = playerTransform.localScale;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (movingForward)
+        {
+            ScaleIncreaser(playerScale.x);
+            ScaleIncreaser(playerScale.y);
+        }
+        else
+        {
+            ScaleDecreaser(playerScale.x);
+            ScaleDecreaser(playerScale.y);
+        }
+        
+    }
+
+    private float ScaleIncreaser(float scaleUp)
+    {
+        return scaleUp++;
+    }
+
+    private float ScaleDecreaser(float scaleDown)
+    {
+        return scaleDown--;
+    }
+}
