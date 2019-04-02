a777f104847570b9830d78b11e252ed47b957fcd
Kaden Campbell <10836246@my.uvu.edu>
2019-04-01 21:32:02 -0600

    Added WeaponSwitch

---
+++ 1610FirstGame/Assets/Scripts/DailyScripts/WeaponSwitch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class WeaponSwitch : ScriptableObject
+{
+    public UnityEvent pistolEvent, rifleEvent, crossbowEvent;
+    
+    public enum WeaponStates
+    {
+        Pistol,
+        Rifle,
+        Crossbow
+    }
+
+    public WeaponStates myWeapon;
+
+    public void ChangeToPistol()
+    {
+        myWeapon = WeaponStates.Pistol;
+    }
+    
+    public void ChangeToRifle()
+    {
+        myWeapon = WeaponStates.Rifle;
+    }
+    
+    public void ChangeToCrossbow()
+    {
+        myWeapon = WeaponStates.Crossbow;
+    }
+
+    public void ChangeWeapon()
+    {
+        switch (myWeapon)
+        {
+            case WeaponStates.Pistol:
+                pistolEvent.Invoke();
+                break;
+            case WeaponStates.Rifle:
+                rifleEvent.Invoke();
+                break;
+            case WeaponStates.Crossbow:
+                crossbowEvent.Invoke();
+                break;
+            default:
+                break;
+        }
+    }
+
+}
