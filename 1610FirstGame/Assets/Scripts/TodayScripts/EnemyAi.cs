b56089bc57caa7ba88bb87c53f09c24e0874858f
Kaden <10836246@my.uvu.edu>
2019-04-08 11:15:03 -0600

    learned Ai and NavMesh

---
+++ 1610FirstGame/Assets/Scripts/TodayScripts/EnemyAi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+//    // Update is called once per frame
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        agent.destination = player.position;
+//    }
+//
+//    private void OnTriggerExit(Collider other)
+//    {
+//        agent.destination = transform.position;
+//    }
+    private void Update()
+    {
+        agent.destination = player.position;
+    }
+}
