fbc50bce81c447d660c6b72e2dea84c280b28395
Kaden <10836246@my.uvu.edu>
2019-04-17 10:54:15 -0600

    Learned more Animation

---
+++ 1610FirstGame/Assets/Scripts/TodayScripts/RunAnims.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAnims : MonoBehaviour
+{
+    private Animator anims;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anims = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anims.SetFloat("Speed", speed);
+        
+        //replacing speed with
+        //CharacterController.velocity
+        //floatData.Value
+    }
+}
