6d9be3e69f0efd7f91264c23559e13253891d0cc
Kaden <10836246@my.uvu.edu>
2019-04-22 11:47:01 -0600

    Finished pizza box and cave floor; started boss stuff

--- FinalGame1610/Assets/Scripts/ItemDrop.cs
+++ FinalGame1610/Assets/Scripts/ItemDrop.cs
@@ -3,12 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+//Integrated from Unity's script reference page
 public class ItemDrop : MonoBehaviour
 {
     public ListOfThings myList;
+    public int itemLife;
+    public int rngReturn;
+    private float counter;
 
-    private void OnCollisionEnter(Collision other)
+
+    private void OnTriggerEnter(Collider other)
     {
-        //gameObject. = myList.collectionList[Random.Range(0, myList.collectionList.Count)];
+        itemLife --;
+        if (itemLife <= 0)
+        {
+            ObjInstance();
+            gameObject.SetActive(false);
+        }
+    }
+
+    
+    
+    //make a switch with preset items that the rngNumber gives a number and then it is that switches preset that the foreach can generate for each obj in said switch
+    
+    
+    
+    public void ObjInstance()
+    {
+        foreach (var myObj in myList.collectionList)
+        {
+            counter++;
+            Instantiate(myObj, new Vector3(counter * 2.0f, 0, 0), Quaternion.identity);
+        }
+    }
+
+    public int rngNumberMaker(int numTotal)
+    {
+        for (int i = 0; i < numTotal + 1; i++)
+        {
+             rngReturn = Random.Range(0, myList.collectionList.Count);
+        }
+
+        return rngReturn;
     }
 }
