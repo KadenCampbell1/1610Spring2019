2bb6c3bac678f9d9f33a0394a4e0ab738366f01b
Kaden Campbell <10836246@my.uvu.edu>
2019-04-22 23:45:09 -0600

    Made Character Animation
    need to remove properties from PlayerBody and make objects childs of the torso and redo the values. (take pictures of values before)

---
+++ FinalGame1610/Assets/Scripts/CharacterFlip.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFlip : MonoBehaviour
+{
+//    public GameObject punchObj;
+//    public GameObject defObj;
+    public GameObject playerAnimSprites;
+    public bool canFlipPunch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canFlipPunch = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("x"))
+        {
+            if (canFlipPunch)
+            {
+                //punchObj.transform.Rotate(0,180,0);
+                //defObj.transform.Rotate(0,180,0);
+                playerAnimSprites.transform.Rotate(0,180,0);
+                canFlipPunch = false;
+            }
+        }
+
+        if (Input.GetKeyDown("c"))
+        {
+            if (!canFlipPunch)
+            {
+                //punchObj.transform.Rotate(0, 180, 0);
+                //defObj.transform.Rotate(0,180,0);
+                playerAnimSprites.transform.Rotate(0,180,0);
+                canFlipPunch = true;
+            }
+        }
+    }
+}
--- FinalGame1610/Assets/Scripts/CharacterMover.cs
+++ FinalGame1610/Assets/Scripts/CharacterMover.cs
@@ -10,10 +10,10 @@ public class CharacterMover : MonoBehaviour
 {
     public MoveBase myCharacterMover;
     public CharacterController myController;
-    public SpriteRenderer mySpriteRenderer;
-    public GameObject punchObj;
-    public GameObject defObj;
-    public bool canFlipPunch;
+    //public SpriteRenderer mySpriteRenderer;
+    //public GameObject punchObj;
+    //public GameObject defObj;
+    //public bool canFlipPunch;
     //public Camera myCamera;
     //public bool flipCamera;
     
@@ -21,8 +21,8 @@ public class CharacterMover : MonoBehaviour
     void Start()
     {
         myController = GetComponent<CharacterController>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
-        canFlipPunch = true;
+        //mySpriteRenderer = GetComponent<SpriteRenderer>();
+        //canFlipPunch = true;
         //myCamera = GetComponent<Camera>();
     }
 
@@ -35,30 +35,30 @@ public class CharacterMover : MonoBehaviour
 //            myCamera.transform.position.x = -myCamera.transform.position.x;
 //        }
 
-        if (Input.GetKeyDown("x"))
-        {
-            mySpriteRenderer.flipX = true;
-            if (canFlipPunch)
-            {
-                punchObj.transform.Rotate(0,180,0);
-                defObj.transform.Rotate(0,180,0);
-                canFlipPunch = false;
-            }
-            //flipCamera = true;
-            //gameObject.transform.Rotate(0,180,0);
-        }
+        //if (Input.GetKeyDown("x"))
+        //{
+        //    mySpriteRenderer.flipX = true;
+        //    if (canFlipPunch)
+        //    {
+        //        punchObj.transform.Rotate(0,180,0);
+        //        defObj.transform.Rotate(0,180,0);
+        //        canFlipPunch = false;
+        //    }
+        //    //flipCamera = true;
+        //    //gameObject.transform.Rotate(0,180,0);
+        //}
 
-        if (Input.GetKeyDown("c"))
-        {
-            mySpriteRenderer.flipX = false;
-            if (!canFlipPunch)
-            {
-                punchObj.transform.Rotate(0, 180, 0);
-                defObj.transform.Rotate(0,180,0);
-                canFlipPunch = true;
-            }
-            //flipCamera = false;
-        }
+        //if (Input.GetKeyDown("c"))
+        //{
+        //    mySpriteRenderer.flipX = false;
+        //    if (!canFlipPunch)
+        //    {
+        //        punchObj.transform.Rotate(0, 180, 0);
+        //        defObj.transform.Rotate(0,180,0);
+        //        canFlipPunch = true;
+        //    }
+        //    //flipCamera = false;
+        //}
         
         myCharacterMover.Move(myController);
     }
