5cbd4438303f50f033cfdfb82d1ad6ee8d92868f
Kaden <10836246@my.uvu.edu>
2019-04-24 11:46:23 -0600

    Fixed Animation and stuff

--- FinalGame1610/Assets/Scripts/BackgroundChanger.cs
+++ FinalGame1610/Assets/Scripts/BackgroundChanger.cs
@@ -3,80 +3,55 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.Experimental.XR;
 using UnityEngine.UI;
 //Integrated from school
 public class BackgroundChanger : MonoBehaviour
 {
-    public BackgroundValues myBackgroundValues;
-    public Image myBackgroundImage;
-    public GameObject myTriggerObject;
-    
-    
-    //this trigger needs to be on triggerObject 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.name == "Player" && myBackgroundValues.backgroundMoving)
-        {
-            if (Input.GetAxis("Horizontal") > 0 && !myBackgroundValues.originalColor)
-            {
-                myBackgroundValues.backgroundColorInt += 10;
-            }
+    public Sprite[] backgroundSprites;
+    public SpriteRenderer myRenderer;
+    public GameObject myPlayer;
 
-            if (Input.GetAxis("Horizontal") < 0 && !myBackgroundValues.originalColor)
-            {
-                myBackgroundValues.backgroundColorInt -= 10;
-            }
+
+    private void Start()
+    {
+        myRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public enum currentBackground
+    {
+        Hills,
+        Cave
+    }
+
+    public currentBackground myBackground;
+    
+    private void Update()
+    {
+        switch (myBackground)
+        {
+            case currentBackground.Hills:
+                myRenderer.sprite = backgroundSprites[0];
+                transform.position = new Vector3(myPlayer.transform.position.x, myPlayer.transform.position.y - 3.9f, myPlayer.transform.position.z + 25);
+                transform.localScale = new Vector3(1, 1, 1);
+                break;
+            case currentBackground.Cave:
+                myRenderer.sprite = backgroundSprites[1];
+                transform.position = new Vector3(myPlayer.transform.position.x - 0.0309f, myPlayer.transform.position.y + 0.04824f, myPlayer.transform.position.z + 25);
+                transform.localScale = new Vector3(1.27551f, 1.27551f, 1.27551f);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
         }
     }
 
-    private void FixedUpdate()
+    public void ChangeBgToCave()
     {
-
-        //have bool to check if it changes to a new name which makes it stop then on trigger exit to make it false
-        //overlap the triggers to make sure the colors dont continue to change
-        switch (myBackgroundValues.myName)
-        {
-            case BackgroundValues.MyBackgroundName.Foreground:
-                myBackgroundValues.originalBlue = 255;
-                myBackgroundValues.originalGreen = 255;
-                myBackgroundValues.originalRed = 255;
-                break;
-            case BackgroundValues.MyBackgroundName.Midground:
-                myBackgroundValues.originalRed = 142;
-                myBackgroundValues.originalGreen = 154;
-                myBackgroundValues.originalBlue = 200;
-                break;
-            case BackgroundValues.MyBackgroundName.Background:
-                myBackgroundValues.originalRed = 95;
-                myBackgroundValues.originalGreen = 105;
-                myBackgroundValues.originalBlue = 144;
-                break;
-        }
-        
-        
-        var backgroundBlue = myBackgroundImage.color.b;
-        var backgroundGreen = myBackgroundImage.color.g;
-        var backgroundRed = myBackgroundImage.color.r;
-        backgroundBlue = myBackgroundValues.backgroundColorInt;
-        backgroundGreen = myBackgroundValues.backgroundColorInt;
-        backgroundRed = myBackgroundValues.backgroundColorInt;
-
-        if (backgroundBlue == 144)
-        {
-            myBackgroundValues.originalColor = true;
-            backgroundBlue = 144;
-        }
-
-        if (backgroundGreen == 105)
-        {
-            myBackgroundValues.originalColor = true;
-            backgroundGreen = 105;
-        }
-
-        if (backgroundRed == 95)
-        {
-            myBackgroundValues.originalColor = true;
-            backgroundRed = 95;
-        }
+        myBackground = currentBackground.Cave;
+    }
+    
+    public void ChangeBgToHill()
+    {
+        myBackground = currentBackground.Hills;
     }
 }
